037120f0fd9df559bfd2d80cb475c308a8ef2be4
Vagrant Default User <vagrant@buster.localdomain>
2021-05-29 11:46:01 +0000

    mannen!

--- mannen.js
+++ mannen.js
@@ -1,5 +1,10 @@
 var express = require("express");
+var app = express();
 
-var app = express();app.listen(3000, () => {
- console.log("Server running on port 3000");
+app.listen(3000, () => {
+ console.log("port:3000");
+});
+
+app.get("/mannen", (req, res, next) => {
+ res.json({ "version": "0.0.1" });
 });
